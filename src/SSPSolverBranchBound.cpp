8c62215e4b42d231e2d03f11f6846e6a0dd16bb4
caio-ggomes <caio.graca@gmail.com>
2020-06-16 10:33:13 -0300

    First commit

---
+++ src/SSPSolverBranchBound.cpp
@@ -0,0 +1,11 @@
+#include <SubsetSumSolver.h>
+
+
+
+ bool SSPSolverBranchBound::solve(const std::vector< long> &input,
+                            long value, std::vector< char> &output) {
+    
+    
+    return false;
+    
+}
